c24230645bc18751c0e68a7ecd7855a36364f035
sx00xs <185215821@qq.com>
2019-11-09 20:00:02 +0800

    01_04将Div变成红色
    原作是使用循环进行更改，修改为动态添加class

---
+++ 4/precache-manifest.3b6d0bf0db19047d7876d5936b2937a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ccd81b3ef9d6e312b8ec6655bfdfd69",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5f51c8ed650660975ef6",
+    "url": "./static/css/main.c433ad74.chunk.css"
+  },
+  {
+    "revision": "657b498defbb279bdbfd",
+    "url": "./static/js/2.3e4e9da8.chunk.js"
+  },
+  {
+    "revision": "5f51c8ed650660975ef6",
+    "url": "./static/js/main.a1bbcad1.chunk.js"
+  },
+  {
+    "revision": "62c5b51341087ef56d17",
+    "url": "./static/js/runtime-main.b71c1fa4.js"
+  }
+]);
